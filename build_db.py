fe19470940c2344807733988e70006c3cfaf85e1
rdzanyM <michalrdzany@gmail.com>
2021-04-13 12:10:50 +0200

    removed hardcoded columns

--- build_db.py
+++ build_db.py
@@ -42,27 +42,7 @@ def conference_to_db(engine, data_path):
 
 
 def journal_to_db(engine, data_path):
-    journals = pd.DataFrame(columns=['Tytuł 1', 'issn', 'e-issn', 'Tytuł 2', 'issn 2', 'e-issn 2', 'Punkty',
-                                     'archeologia', 'filozofia', 'historia', 'językoznawstwo',
-                                     'literaturoznawstwo', 'nauki o kulturze i religii', 'nauki o sztuce',
-                                     'architektura i urbanistyka',
-                                     'automatyka, elektronika i elektrotechnika',
-                                     'informatyka techniczna i telekomunikacja', 'inżynieria biomedyczna',
-                                     'inżynieria chemiczna', 'inżynieria lądowa i transport',
-                                     'inżynieria materiałowa', 'inżynieria mechaniczna',
-                                     'inżynieria środowiska, górnictwo i energetyka', 'nauki farmaceutyczne',
-                                     'nauki medyczne', 'nauki o kulturze fizycznej', 'nauki o zdrowiu',
-                                     'nauki leśne', 'rolnictwo i ogrodnictwo',
-                                     'technologia żywności i żywienia', 'weterynaria',
-                                     'zootechnika i rybactwo', 'ekonomia i finanse',
-                                     'geografia społeczno-ekonomiczna i gospodarka przestrzenna',
-                                     'nauki o bezpieczeństwie', 'nauki o komunikacji społecznej i mediach',
-                                     'nauki o polityce i administracji', 'nauki o zarządzaniu i jakości',
-                                     'nauki prawne', 'nauki socjologiczne', 'pedagogika', 'prawo kanoniczne',
-                                     'psychologia', 'astronomia', 'informatyka', 'matematyka',
-                                     'nauki biologiczne', 'nauki chemiczne', 'nauki fizyczne',
-                                     'nauki o Ziemi i środowisku', 'nauki teologiczne', 'date'])
-
+    journals = None
     for filename in os.listdir(data_path):
         j = pd.read_excel(os.path.join(data_path, filename), 0, header=0)
         j = j.iloc[:, 1:]
@@ -81,13 +61,16 @@ def journal_to_db(engine, data_path):
         j.loc[j['e-issn'].isna(), 'e-issn'] = j[j['e-issn'].isna()]['e-issn 2']
 
         j['date'] = filename[-15:-5]
-        journals = journals.append(j)
+        if journals is None:
+            journals = j
+        else:
+            journals = journals.append(j)
 
     titles = pd.DataFrame(columns=['id', 'title'])
     titles['title'] = journals['Tytuł 1'].unique()
     titles['id'] = titles.index
     joined = titles.join(journals.set_index('Tytuł 1'), on='title', how='outer')
-    dates = joined[['id','date','points']]
+    dates = joined[['id', 'date', 'points']]
     dates.columns = ['journal_id', 'starting_date', 'points']
     titles.to_sql(name='Journals', con=engine, if_exists='append', index=False, index_label='id')
     dates.to_sql(name='JournalDatePoints', con=engine, if_exists='append', index=False, index_label=None)

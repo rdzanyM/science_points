d7d3ffcc46bbcda6ae8976d383bd9a1b0e59e302
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2021-05-09 17:57:25 +0200

    Fix the first half of UI

--- src/app_utils.py
+++ src/app_utils.py
@@ -1,6 +1,9 @@
+from typing import List
+
 import dash_html_components as html
 import colorlover as cl
 import pandas as pd
+
 
 def format_suggestions_based_on_search(searched_term: str, search_result: pd.DataFrame) -> str:
     if len(search_result) == 0:
@@ -9,11 +12,11 @@ def format_suggestions_based_on_search(searched_term: str, search_result: pd.Dat
     if len(search_result) == 1:
         return f'Szukano: *{searched_term}*, znaleziono tylko powyższy wynik.'
 
-
     markdown_value = f'Szukano: *{searched_term}*.\n\n Inne sugestie:'
     for i in range(1, min(len(search_result), 3)):
-        markdown_value+=f'\n{i}. {search_result.name.iloc[i]}'
+        markdown_value += f'\n{i}. {search_result.name.iloc[i]}'
     return markdown_value
+
 
 def format_colors_based_on_similarity():
     scale = cl.scales['5']['div']['RdYlGn']
@@ -47,7 +50,7 @@ def format_colors_based_on_similarity():
             'backgroundColor': scale[2],
             'color': 'black'
         },
-        
+
         {
             'if': {
                 'column_id': 'Similarity',
@@ -57,7 +60,7 @@ def format_colors_based_on_similarity():
             'backgroundColor': scale[1],
             'color': 'black'
         },
-        
+
         {
             'if': {
                 'column_id': 'Similarity',
@@ -68,3 +71,25 @@ def format_colors_based_on_similarity():
             'color': 'white'
         },
     ]
+
+
+def row_col(
+        column_contents: list,
+        column_widths: List[int],
+        column_styles: List[dict] = None,
+        row_extra_classes: str = '',
+        row_style: dict = None,
+):
+    column_styles = column_styles or [dict()] * len(column_contents)
+    return html.Div(
+        className=f'row {row_extra_classes}',
+        style=row_style or dict(),
+        children=[
+            html.Div(
+                className=f'col-{width}',
+                style=style,
+                children=content,
+            )
+            for content, width, style in zip(column_contents, column_widths, column_styles)
+        ]
+    )
